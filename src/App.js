1057441db35589c1593910f3cf1083cd47b06756
prafful0026 <prafful0026@gmail.com>
2020-12-25 20:53:36 +0530

    ADDED A SHOP PAGE , COLLECTION PREVIEW COMPONENT , COLLECTION ITEM COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 import Homepage from "./pages/homepage/homepage-component.jsx";
 import { Route, Switch } from "react-router-dom";
-
-const hats = () => {
-  return (
-    <h1>
-      hats page
-    </h1>
-  )
-
-}
+import ShopPage from "./pages/shop/shop-component.jsx";
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/hats" component={hats} />
         <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
 
     </div>
