a1ce6ab6bf3c50bdc2a836a7b552190cf69be3ef
prafful0026 <prafful0026@gmail.com>
2020-12-26 20:02:34 +0530

    Sign in page added with sign in component and custom button component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Homepage from "./pages/homepage/homepage-component.jsx";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop-component.jsx";
 import Header from "./components/header-component/header-component.jsx"
+import  SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up-component.jsx"
 function App() {
   return (
     <div>
       <Header /> 
-      <Switch> 
+      <Switch>
+        <Route exact path="/signin" component={ SignInAndSignUpPage}  />
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
       </Switch>
---
+++ src/components/sign-in/sign-in-component.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import FormInput from "../form-input/form-input-component.jsx";
+import "./sign-in-styles.scss"
+import CustomButton from "../custom-button/custom-button-component.jsx"
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+      handleSubmit=event=>{
+          event.preventDefault();
+          this.setState({email:'',password:''})
+      }
+      handleChange=event=>{
+          const{value,name}=event.target;
+          this.setState({[name]:value})
+      }
+    render() {
+        return (
+            <div onSubmit={this.handleSubmit} className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with ur emain and password</span>
+                <form action="">
+                <FormInput label='email' handleChange={this.handleChange} value={this.state.email} name="email" type="email" required/>
+                <FormInput label="password" handleChange={this.handleChange} value={this.state.password} name="password" type="password" required/>
+                <CustomButton type="submit">Sign In</CustomButton>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default SignIn; 
