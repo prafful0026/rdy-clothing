702e2b6d74f7cbc8b755cf1c82daa94c81ad2ab6
prafful0026 <prafful0026@gmail.com>
2021-01-03 00:51:55 +0530

    revert

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop-component.jsx";
 import Header from "./components/header-component/header-component.jsx"
 import  SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up-component.jsx"
-import { auth } from "./firebase/firebase-utils.js";
+import { auth,createUserProfileDocument } from "./firebase/firebase-utils.js";
 class App extends React.Component {
   constructor(){
     super();
@@ -15,7 +15,7 @@ class App extends React.Component {
   }
   unSubscribeFromAuth=null;
   componentDidMount(){
-  this.unSubscribeFromAuth=  auth.onAuthStateChanged(user=>{this.setState({currentUser:user});})
+  this.unSubscribeFromAuth=  auth.onAuthStateChanged(async user=>{createUserProfileDocument(user)})
   }
   componentWillUnmount(){
     this.unSubscribeFromAuth();
