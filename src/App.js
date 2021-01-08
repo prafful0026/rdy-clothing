dcb2710f109552da7a591e60bb20dd44c0e2a805
prafful0026 <prafful0026@gmail.com>
2021-01-08 18:15:03 +0530

    included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -14,12 +14,30 @@ class App extends React.Component {
     };
   }
   unSubscribeFromAuth=null;
-  componentDidMount(){
-  this.unSubscribeFromAuth=  auth.onAuthStateChanged(async  user=>{createUserProfileDocument(user)})
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
-  componentWillUnmount(){
-    this.unSubscribeFromAuth();
-  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }  
   render(){
     return (
       <div>
